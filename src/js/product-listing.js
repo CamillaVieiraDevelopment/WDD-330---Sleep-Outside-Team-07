2ee97ba886ee16aa15f5cf6faa7ca357e342fb69
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2026-05-23 11:15:34 -0300

    initial commit

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,19 +4,14 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
-// Captura a categoria vinda da URL
 const category = getParam("category");
 
-// STEP 8: Atualiza dinamicamente o título H2 da página
 if (category) {
-    // Captura o elemento h2 da página (usando a classe que adicionamos no HTML)
     const titleElement = document.querySelector(".title-heading");
 
     if (titleElement) {
-        // Formata o texto para deixar a primeira letra maiúscula (ex: tents -> Tents)
         const formattedCategory = category.charAt(0).toUpperCase() + category.slice(1);
 
-        // Altera o texto para o padrão exigido: "Top Products: Category"
         titleElement.textContent = `Top Products: ${formattedCategory}`;
     }
 }
