ee9034fc39e7c1585400b9c61d100a063b4d7fab
aaron040998 <aaron.a.barra1998@gmail.com>
2026-05-28 09:53:29 -0500

    Add customizable alert to index.html

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,13 @@ import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 import { updateCartCount } from "./cart.js";
+import Alert from "./Alert.js";          
+
+loadHeaderFooter().then(() => {
+  updateCartCount();
+  const alerts = new Alert();
+  alerts.init();
+});
 
 loadHeaderFooter().then(() => {
   updateCartCount();
