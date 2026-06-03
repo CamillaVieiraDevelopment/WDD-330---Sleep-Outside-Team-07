7e7de255ce67a6a0af8e62bef17cff85efb39099
Gamujob <jgamutambuli@byupathway.edu>
2026-06-03 21:53:53 +0300

    week 03

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,14 @@
 // Removed the set/get LocalStorage from here and imported getParam and our new ProductDetails class.
 // Following ES Modules principles, this file only orchestrates dependencies and starts the process.
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
 //Retrieve the ID from the URL (e.g., 880RR)
-const productId = getParam("product");
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
 
 // Create an instance of our new class, passing the ID and the data access layer
